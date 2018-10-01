7a521b21bc24bc5ba25ab8032f0bf38fb30d38a0
Inferior-Programmer <43639805+Inferior-Programmer@users.noreply.github.com>
2018-10-01 19:23:40 +0800

    repeat

--- main_codee.py
+++ main_codee.py
@@ -1,7 +1,7 @@
 import sequence_2
 import threading
 def call_funct():
-    threading.Timer(40.0, call_funct).start()
+    threading.Timer(70, call_funct).start()
     sequence_2.create_file()
 
 call_funct()
