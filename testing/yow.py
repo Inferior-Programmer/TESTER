1bbab724863cb04c8f9aed2a0735895ae27e0290
Inferior-Programmer <43639805+Inferior-Programmer@users.noreply.github.com>
2018-10-01 18:06:15 +0800

    repeat

---
+++ testing/yow.py
@@ -0,0 +1,4 @@
+f = open('helloworld.txt' ,'w+')
+f.write('world\n')
+f.write('LOL\n')
+f.close()
---
+++ trynatin.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('git add .')
+os.system('git commit -m "repeat" ')
+os.system('git push -u origin master')
